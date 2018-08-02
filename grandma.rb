d985f583e4becad4ca8e515519328fd19e7c9c12
Sav Younts <savyounts@gmail.com>
2018-08-02 22:37:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
+  else
     return "NO, NOT SINCE 1938!"
   
   end 
